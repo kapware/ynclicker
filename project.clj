30a45eb5a112933ceeb0dd1f498741d14fa69095
Pawel Kapala <git@kapware.com>
2016-10-11 22:48:15 +0200

    Cider support

--- project.clj
+++ project.clj
@@ -3,7 +3,26 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [metosin/compojure-api "1.1.8"]
                  [alandipert/enduro "1.2.0"]]
+  :min-lein-version "2.6.1"
   :ring {:handler ynclicker.handler/app}
-  :uberjar-name "server.jar"
-  :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]]
-                   :plugins [[ikitommi/lein-ring "0.9.8-FIX"]]}})
+  :uberjar-name "ynclicker.jar"
+  :profiles {:uberjar {:aot :all
+                       :main clj-krakus.main}
+             :dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]
+                                  [cider/cider-nrepl "0.14.0-SNAPSHOT"]]
+                   :plugins [[ikitommi/lein-ring "0.9.8-FIX"]]
+                   :ring {:handler ynclicker.handler/app
+                          :nrepl {:start? true}}
+                   :repl-options {:nrepl-middleware
+                                  [cider.nrepl.middleware.apropos/wrap-apropos
+                                   cider.nrepl.middleware.classpath/wrap-classpath
+                                   cider.nrepl.middleware.complete/wrap-complete
+                                   cider.nrepl.middleware.info/wrap-info
+                                   cider.nrepl.middleware.inspect/wrap-inspect
+                                   cider.nrepl.middleware.macroexpand/wrap-macroexpand
+                                   cider.nrepl.middleware.ns/wrap-ns
+                                   cider.nrepl.middleware.resource/wrap-resource
+                                   cider.nrepl.middleware.stacktrace/wrap-stacktrace
+                                   cider.nrepl.middleware.test/wrap-test
+                                   cider.nrepl.middleware.trace/wrap-trace
+                                   cider.nrepl.middleware.undef/wrap-undef]}}})
